cffbb3c93093273d6325a5436cbb51c405a02b18
adithyanvs <adithyanvs@gmail.com>
2022-08-03 16:59:53 +0530

    add

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,20 +2,31 @@ import React from "react";
 import { useState, useEffect } from "react";
 import { Row, Col } from "react-bootstrap";
 import RestaurantCard from "./RestaurantCard";
+import {listRestaurants} from '../actions/restaurantAction'
+import {useDispatch,useSelector} from 'react-redux'
 
 function Home() {
-  const [restaurants, setRestaurants] = useState([]);
-  async function fetchData() {
-    await fetch("./restaurants.json")
-      .then((data) => data.json())
-      .then((res) => setRestaurants(res.restaurants));
-  }
-  useEffect(() => {
-    fetchData();
-  }, []);
+// const[filter,setFIlter] =useState("");
+const dispatch =useDispatch()
+const restaurantData = useSelector(state =>state.restaurantReducer)
+const {restaurants} =restaurantData
+useEffect(() =>{
+  dispatch(listRestaurants())
+},[]);
 
-  console.log("my data is", restaurants);
+  // const [restaurants, setRestaurants] = useState([]);
+  // async function fetchData() {
+  //   await fetch("./restaurants.json")
+  //     .then((data) => data.json())
+  //     .then((res) => setRestaurants(res.restaurants));
+  // }
+  // useEffect(() => {
+  //   fetchData();
+  // }, []);
+
+  // console.log("my data is", restaurants);
   return (
+    <>
     <Row>
       {restaurants.map((item) => (
         <Col sm={12} md={8} lg={6} xl={3}>
@@ -23,6 +34,7 @@ function Home() {
         </Col>
       ))}
     </Row>
+    </>
   );
 }
 
